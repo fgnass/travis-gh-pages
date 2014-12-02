a9e4c122af075a78bbf7b42250c710038dd7238b
Felix Gnass <fgnass@gmail.com>
2014-12-02 16:37:33 +0100

    remove branch name

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd gh-pages
 git checkout gh-pages
 date >> index.html
 git add . && commit -m "Deployed to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1

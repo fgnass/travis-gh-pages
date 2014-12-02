12d3c6814ab3e2f8abeac091af188c2d36e809e9
Felix Gnass <fgnass@gmail.com>
2014-12-02 13:18:43 +0100

    add encrypted access token

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-set
-
 git clone . gh-pages
 cd gh-pages
 git checkout gh-pages
 date >> index.html
 git add . && commit -m "Deployed to GitHub Pages"
-git remote add github https://github.com/fgnass/travis-gh-pages.git
-git push github gh-pages
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1

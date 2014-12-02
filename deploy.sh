90c034e69bcaf6048ee710a60409572ad7131973
Felix Gnass <fgnass@gmail.com>
2014-12-02 10:18:24 +0100

    use https url so we can push

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+set
+
 git clone . gh-pages
 cd gh-pages
 git checkout gh-pages
 date >> index.html
 git add . && commit -m "Deployed to GitHub Pages"
-cp ../.git/config .git
-git push origin gh-pages
+git remote add github https://github.com/fgnass/travis-gh-pages.git
+git push github gh-pages

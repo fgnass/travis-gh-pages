dddd6683db2b59c6f769c3974fafffabdd7627a5
Felix Gnass <fgnass@gmail.com>
2014-12-02 23:28:01 +0100

    master:gh-pages

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git config user.name "Travis-CI"
 git config user.email "travis@travis-ci.org"
 git add .
 git commit -m "Deployed to GitHub Pages"
-git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1

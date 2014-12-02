6bcd38b84a3da6ed28942149c506df9fcc00eb82
Felix Gnass <fgnass@gmail.com>
2014-12-02 17:00:58 +0100

    add missing 'git'

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ git clone . gh-pages
 cd gh-pages
 git checkout gh-pages
 date >> index.html
-git add . && commit -m "Deployed to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
+git add . && git commit -m "Deployed to GitHub Pages"
+git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
